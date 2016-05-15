80f7843ab4be5502ea8b0e355b51c28e9e9568ea
Victor Zerefos <zvelopswebsite@gmail.com>
2016-05-15 17:25:55 -0300

    created mailer contact and setup contact controller for mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base
+	default to: 'zvelopswebsite@gmail.com'
+	def contact_mail(name,email,body)
+		@name   = name
+		@email  = email
+		@body	  = body	
+
+		mails(from: email, subject: 'Contact Form Message')
+	end
+end
