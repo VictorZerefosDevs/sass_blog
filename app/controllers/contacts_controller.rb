fcd8f590fae542c68dbd417c98d7d413fa409f77
Victor Zerefos <zvelopswebsite@gmail.com>
2016-05-15 16:56:50 -0300

    added index action and validates de contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,28 @@
 class ContactsController < ApplicationController
 
+	def index
+		@contacts = Contact.all
+	end
+
 	def new
 		@contact = Contact.new
 	end
 
 	def create
 		@contact = Contact.new(contact_params)
-		@contact.save
+		if @contact.save 
+			flash[:success] = "Message sent."
+			redirect_to new_contact_path
+		else
+			flash[:danger] = "Something went wrong."
+			redirect_to new_contact_path
+		end
 	end
 
 private
-
-def contact_params
-	params.require(:contact).permit(:name, :email, :comments)
-end
+	def contact_params
+		params.require(:contact).permit(:name, :email, :comments)
+	end
 
 
 end
